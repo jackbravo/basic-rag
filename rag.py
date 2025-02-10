65b2e5e6fbc9deea98b5b1cc3813ea1fdb947cb1
Joaquin Bravo Contreras <jackbravo@gmail.com>
2025-02-10 14:45:55 -0600

    extract text

--- rag.py
+++ rag.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from rag-challenge!")
+import typer
+import pymupdf4llm
+from semantic_text_splitter import MarkdownSplitter
+
+
+def main(pdf_path: str):
+    md_text = pymupdf4llm.to_markdown(pdf_path)
+    splitter = MarkdownSplitter(1000)
+    chunks = splitter.chunks(md_text)
+    print(chunks)
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
